946561bf6298bdf3c258d820d4f1b2d02204a7b0
filip <filip.raszka3@gmail.com>
2020-05-02 23:29:10 +0200

    test fridge book

--- fridge/settings.py
+++ fridge/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fridge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'application/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
